b033b70d684fe6e523104b9a4707d79a5f4f7fd0
BuildTools <unconfigured@null.spigotmc.org>
2024-12-11 23:16:08 +0900

    Fabric API 필요없게 수정

---
+++ src/main/java/com/github/cidite/mixin/CommandManagerMixin.java
@@ -0,0 +1,30 @@
+package com.github.cidite.mixin;
+
+
+import com.github.cidite.commands.*;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(CommandManager.class)
+public class CommandManagerMixin {
+
+    private final CommandDispatcher<ServerCommandSource> dispatcher = new CommandDispatcher();
+
+    @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/server/command/CommandManager$RegistrationEnvironment;Lnet/minecraft/command/CommandRegistryAccess;)V")
+    private void init(CommandManager.RegistrationEnvironment environment, CommandRegistryAccess commandRegistryAccess, CallbackInfo info) {
+			ChanceCommand.register(this.dispatcher);
+			GetposCommand.register(this.dispatcher);
+			GetrotationCommand.register(this.dispatcher);
+			GettickCommand.register(this.dispatcher);
+			PosCommand.register(this.dispatcher);
+			RotCommand.register(this.dispatcher);
+    }
+
+}
