09c3ccc212be7419dd2d80dda6bbf6b49c6831ba
BuildTools <unconfigured@null.spigotmc.org>
2025-01-31 18:04:12 +0900

    get_pos 명령어 코드 정리

--- src/main/java/com/github/cidite/commands/GetposCommand.java
+++ src/main/java/com/github/cidite/commands/GetposCommand.java
@@ -2,7 +2,6 @@ package com.github.cidite.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
@@ -11,111 +10,98 @@ import net.minecraft.text.Text;
 
 // /get_pos <X|Y|Z> [스케일] [대상]
 // 개체의 좌표 X/Y/Z를 출력하는 명령어.
-
 public class GetposCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register((LiteralArgumentBuilder) ((LiteralArgumentBuilder)
+        dispatcher.register(
                 CommandManager.literal("get_pos")
-                        .requires(source -> source.hasPermissionLevel(2)))
-                .then(
-                        CommandManager.literal("x").then(CommandManager
-                                        .argument("scale", DoubleArgumentType.doubleArg())
-                                        .then(CommandManager
-                                                .argument("target", EntityArgumentType.entity())
-                                                .executes((context) -> {
-                                                    return executeScale(
-                                                                    context.getSource(),
-                                                            EntityArgumentType.getEntity(context, "target"),
-                                                            EntityArgumentType.getEntity(context, "target").getX(),
-                                                            DoubleArgumentType.getDouble(context, "scale"));
-                                                })
-                                        )
-                                        .executes((context) -> {
-                                            return executeScale(
-                                                            context.getSource(),
-                                                    (context.getSource()).getEntity(),
-                                                    (context.getSource()).getEntity().getX(),
-                                                    DoubleArgumentType.getDouble(context, "scale"));
-                                        })
+                        .requires(source -> source.hasPermissionLevel(2))
+                .then(CommandManager.literal("x")
+                        .then(CommandManager
+                                .argument("scale", DoubleArgumentType.doubleArg())
+                                .then(CommandManager
+                                        .argument("target", EntityArgumentType.entity())
+                                        .executes(context -> executeScale(
+                                                context.getSource(),
+                                                EntityArgumentType.getEntity(context, "target"),
+                                                EntityArgumentType.getEntity(context, "target").getX(),
+                                                DoubleArgumentType.getDouble(context, "scale")))
                                 )
-                                .executes((context) -> {
-                                    return execute(
-                                                    context.getSource(),
-                                            (context.getSource()).getEntity(),
-                                            (context.getSource()).getEntity().getX());
-                                })
-                ).then(
-                        CommandManager.literal("y").then(CommandManager
-                                        .argument("scale", DoubleArgumentType.doubleArg())
-                                        .then(CommandManager
-                                                .argument("target", EntityArgumentType.entity())
-                                                .executes((context) -> {
-                                                    return executeScale(
-                                                                    context.getSource(),
-                                                            EntityArgumentType.getEntity(context, "target"),
-                                                            EntityArgumentType.getEntity(context, "target").getY(),
-                                                            DoubleArgumentType.getDouble(context, "scale"));
-                                                })
-                                        )
-                                        .executes((context) -> {
-                                            return executeScale(
-                                                            context.getSource(),
-                                                    (context.getSource()).getEntity(),
-                                                    (context.getSource()).getEntity().getY(),
-                                                    DoubleArgumentType.getDouble(context, "scale"));
-                                        })
+                                .executes(context -> executeScale(
+                                        context.getSource(),
+                                        (context.getSource()).getEntity(),
+                                        (context.getSource()).getEntity().getX(),
+                                        DoubleArgumentType.getDouble(context, "scale"))
                                 )
-                                .executes((context) -> {
-                                    return execute(
-                                                    context.getSource(),
-                                            (context.getSource()).getEntity(),
-                                            (context.getSource()).getEntity().getY());
-                                })
-                ).then(
-                        CommandManager.literal("z").then(CommandManager
-                                        .argument("scale", DoubleArgumentType.doubleArg())
-                                        .then(CommandManager
-                                                .argument("target", EntityArgumentType.entity())
-                                                .executes((context) -> {
-                                                    return executeScale(
-                                                                    context.getSource(),
-                                                            EntityArgumentType.getEntity(context, "target"),
-                                                            EntityArgumentType.getEntity(context, "target").getZ(),
-                                                            DoubleArgumentType.getDouble(context, "scale"));
-                                                })
+                        )
+                        .executes(context -> execute(
+                                context.getSource(),
+                                (context.getSource()).getEntity(),
+                                (context.getSource()).getEntity().getX())
+                        )
+                )
+                .then(CommandManager.literal("y")
+                        .then(CommandManager
+                                .argument("scale", DoubleArgumentType.doubleArg())
+                                .then(CommandManager
+                                        .argument("target", EntityArgumentType.entity())
+                                        .executes(context -> executeScale(
+                                                context.getSource(),
+                                                EntityArgumentType.getEntity(context, "target"),
+                                                EntityArgumentType.getEntity(context, "target").getY(),
+                                                DoubleArgumentType.getDouble(context, "scale"))
                                         )
-                                        .executes((context) -> {
-                                            return executeScale(
-                                                            context.getSource(),
-                                                    (context.getSource()).getEntity(),
-                                                    (context.getSource()).getEntity().getZ(),
-                                                    DoubleArgumentType.getDouble(context, "scale"));
-                                        })
                                 )
-                                .executes((context) -> {
-                                    return execute(
-                                                    context.getSource(),
-                                            (context.getSource()).getEntity(),
-                                            (context.getSource()).getEntity().getZ());
-                                })
+                                .executes(context -> executeScale(
+                                        context.getSource(),
+                                        (context.getSource()).getEntity(),
+                                        (context.getSource()).getEntity().getY(),
+                                        DoubleArgumentType.getDouble(context, "scale"))
+                                )
+                        )
+                        .executes(context -> execute(
+                                context.getSource(),
+                                (context.getSource()).getEntity(),
+                                (context.getSource()).getEntity().getY())
+                        )
+                )
+                .then(CommandManager.literal("z")
+                        .then(CommandManager
+                                .argument("scale", DoubleArgumentType.doubleArg())
+                                .then(CommandManager
+                                        .argument("target", EntityArgumentType.entity())
+                                        .executes(context -> executeScale(
+                                                context.getSource(),
+                                                EntityArgumentType.getEntity(context, "target"),
+                                                EntityArgumentType.getEntity(context, "target").getZ(),
+                                                DoubleArgumentType.getDouble(context, "scale"))
+                                        )
+                                )
+                                .executes(context -> executeScale(
+                                        context.getSource(),
+                                        (context.getSource()).getEntity(),
+                                        (context.getSource()).getEntity().getZ(),
+                                        DoubleArgumentType.getDouble(context, "scale"))
+                                )
+                        )
+                        .executes(context -> execute(
+                                context.getSource(),
+                                (context.getSource()).getEntity(),
+                                (context.getSource()).getEntity().getZ())
+                        )
                 )
         );
     }
 
     //실행시 값을 확인함.
     private static int execute(ServerCommandSource source, Entity entity, double value) {
-        source.sendFeedback(() -> {
-            return Text.translatable("commands.data.entity.query", entity.getDisplayName(), value);
-        }, true);
+        source.sendFeedback(() -> Text.translatable("commands.data.entity.query", entity.getDisplayName(), value), true);
         return (int) value;
     }
     //값 확인 +스케일 곱셈
     private static int executeScale(ServerCommandSource source, Entity entity, double value, double scale) {
         value *= scale;
         double i = value;
-        source.sendFeedback(() -> {
-            return Text.translatable("commands.data.entity.query", entity.getDisplayName(), i);
-        }, true);
+        source.sendFeedback(() -> Text.translatable("commands.data.entity.query", entity.getDisplayName(), i), true);
         return (int) i;
     }
 
