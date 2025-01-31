8b696b8462177a43639d534744d55f0b54c9c3dd
BuildTools <unconfigured@null.spigotmc.org>
2025-01-31 18:05:52 +0900

    rot 명령어 코드 정리

--- src/main/java/com/github/cidite/commands/RotCommand.java
+++ src/main/java/com/github/cidite/commands/RotCommand.java
@@ -1,7 +1,6 @@
 package com.github.cidite.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.command.argument.PosArgument;
 import net.minecraft.command.argument.RotationArgumentType;
@@ -13,41 +12,31 @@ import net.minecraft.util.math.Vec2f;
 
 public class RotCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register((LiteralArgumentBuilder)((LiteralArgumentBuilder)
+        dispatcher.register(
                 CommandManager.literal("rot")
-                        .requires(source -> source.hasPermissionLevel(2)))
-                .then(
-                        (CommandManager
-                                .argument("target", EntityArgumentType.entity())
-                                .then(CommandManager
-                                        .argument("rotation", RotationArgumentType.rotation())
-                                        .executes((context) -> {
-                                                    return execute(
-                                                            context.getSource(),
-                                                            EntityArgumentType.getEntity(context, "target"),
-                                                            RotationArgumentType.getRotation(context,"rotation"));
-                                                }
-                                        )
-                                )
-                        )
-                )
-                .then(
-                        (CommandManager
+                        .requires(source -> source.hasPermissionLevel(2))
+                .then(CommandManager
+                        .argument("target", EntityArgumentType.entity())
+                        .then(CommandManager
                                 .argument("rotation", RotationArgumentType.rotation())
-                                .executes((context) -> {
-                                            return execute(
-                                                    context.getSource(),
-                                                    (context.getSource()).getEntity(),
-                                                    RotationArgumentType.getRotation(context, "rotation"));
-                                        }
+                                .executes(context -> execute(
+                                        context.getSource(),
+                                        EntityArgumentType.getEntity(context, "target"),
+                                        RotationArgumentType.getRotation(context,"rotation"))
                                 )
                         )
                 )
-                .executes((context) -> {
-                            return executeDefault((ServerCommandSource)
-                                            context.getSource(),
-                                    ((ServerCommandSource)context.getSource()).getEntity());
-                        }
+                .then(CommandManager
+                        .argument("rotation", RotationArgumentType.rotation())
+                        .executes(context -> execute(
+                                context.getSource(),
+                                (context.getSource()).getEntity(),
+                                RotationArgumentType.getRotation(context, "rotation"))
+                        )
+                )
+                .executes(context -> executeDefault(
+                        context.getSource(),
+                        (context.getSource()).getEntity())
                 )
         );
     }
@@ -55,18 +44,14 @@ public class RotCommand {
     private static int execute(ServerCommandSource source, Entity entity, PosArgument rotation) {
         Vec2f vec2f = rotation.getRotation(source);
         entity.rotate(vec2f.y, vec2f.x);
-        source.sendFeedback(() -> {
-            return Text.translatable("commands.rotate.success", entity.getDisplayName());
-        }, true);
+        source.sendFeedback(() -> Text.translatable("commands.rotate.success", entity.getDisplayName()), true);
         return 1;
     }
 
     private static int executeDefault(ServerCommandSource source, Entity entity) {
         Vec2f vec2f = source.getRotation();
         entity.rotate(vec2f.y, vec2f.x);
-        source.sendFeedback(() -> {
-            return Text.translatable("commands.rotate.success", entity.getDisplayName());
-        }, true);
+        source.sendFeedback(() -> Text.translatable("commands.rotate.success", entity.getDisplayName()), true);
         return 1;
     }
 
