156f0c17353db4f0949ca2646e90689dcbcff7ca
BuildTools <unconfigured@null.spigotmc.org>
2025-02-26 09:36:41 +0900

    주석 제거

--- src/main/java/com/github/cidite/siditeUtilityMain.java
+++ src/main/java/com/github/cidite/siditeUtilityMain.java
@@ -1,9 +1,6 @@
 package com.github.cidite;
 
-//import com.github.cidite.commands.*;
-
 import net.fabricmc.api.ModInitializer;
-//import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,13 +11,5 @@ public class siditeUtilityMain implements ModInitializer {
     @Override
 	public void onInitialize() {
 		LOGGER.info("Sidite Utility Initialize.");
-//		CommandRegistrationCallback.EVENT.register((dispatcher, commandRegistryAccess, ignored1) -> {
-//			ChanceCommand.register(dispatcher);
-//			GetposCommand.register(dispatcher);
-//			GetrotationCommand.register(dispatcher);
-//			GettickCommand.register(dispatcher);
-//			PosCommand.register(dispatcher);
-//			RotCommand.register(dispatcher);
-//		});
 	}
 }
